a9d39289a8576be4fea7b291b2535bbb2d6dcf4b
edwincarranza <87099939+edwincarranza@users.noreply.github.com>
2021-07-07 18:57:32 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,1 +1,9 @@
-
+firebase.initializeApp({
+   apiKey: "AIzaSyDc0nmHzvMsImJmdk1e7V6Sy85NASuzoMc",
+    authDomain: "autentiedwin.firebaseapp.com",
+    projectId: "autentiedwin",
+    storageBucket: "autentiedwin.appspot.com",
+    messagingSenderId: "820525293988",
+    appId: "1:820525293988:web:ea15f00e27d6796a79a131",
+    measurementId: "G-BKQH0H7Y3G"
+});
